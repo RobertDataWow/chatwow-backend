35d230b9713e7cb131a4101e9226aecb778a7507
Khunakorn Johnson <khunakorn.j@datawow.io>
2025-11-26 13:11:46 +0700

    feat: forgot password security

--- src/app/api/v1/auth/forgot-password/forgot-password.command.ts
+++ src/app/api/v1/auth/forgot-password/forgot-password.command.ts
@@ -1,11 +1,9 @@
 import { PasswordResetToken } from '@domain/base/password-reset-token/password-reset-token.domain';
-import { PasswordResetTokenMapper } from '@domain/base/password-reset-token/password-reset-token.mapper';
 import { PasswordResetTokenService } from '@domain/base/password-reset-token/password-reset-token.service';
 import { User } from '@domain/base/user/user.domain';
 import { UserMapper } from '@domain/base/user/user.mapper';
 import { UserService } from '@domain/base/user/user.service';
 import { usersTableFilter } from '@domain/base/user/user.util';
-import { getAccessToken } from '@domain/orchestration/auth/auth.util';
 import { DomainEventQueue } from '@domain/orchestration/queue/domain-event/domain-event.queue';
 import { Inject, Injectable } from '@nestjs/common';
 
@@ -14,7 +12,6 @@ import { TransactionService } from '@infra/global/transaction/transaction.servic
 
 import { shaHashstring } from '@shared/common/common.crypto';
 import { CommandInterface } from '@shared/common/common.type';
-import { ApiException } from '@shared/http/http.exception';
 import { HttpResponseMapper } from '@shared/http/http.mapper';
 
 import {
@@ -41,6 +38,11 @@ export class ForgotPasswordCommand implements CommandInterface {
 
   async exec(body: ForgotPasswordDto): Promise<ForgotPasswordResponse> {
     const user = await this.find(body);
+    if (!user) {
+      return HttpResponseMapper.toSuccess({
+        data: {},
+      });
+    }
 
     const token = shaHashstring();
     const passwordResetToken = PasswordResetToken.new({
@@ -59,19 +61,9 @@ export class ForgotPasswordCommand implements CommandInterface {
       plainToken: token,
     });
 
+    // for security always success
     return HttpResponseMapper.toSuccess({
-      data: {
-        user: {
-          attributes: UserMapper.toResponse(user),
-          relations: {
-            passwordResetToken: {
-              attributes:
-                PasswordResetTokenMapper.toResponse(passwordResetToken),
-            },
-          },
-        },
-        token: getAccessToken(user),
-      },
+      data: {},
     });
   }
 
@@ -85,7 +77,7 @@ export class ForgotPasswordCommand implements CommandInterface {
     });
   }
 
-  async find(body: ForgotPasswordDto): Promise<User> {
+  async find(body: ForgotPasswordDto): Promise<User | null> {
     const user = await this.readDb
       .selectFrom('users')
       .selectAll()
@@ -95,7 +87,7 @@ export class ForgotPasswordCommand implements CommandInterface {
       .executeTakeFirst();
 
     if (!user) {
-      throw new ApiException(403, 'invalidEmail');
+      return null;
     }
 
     return UserMapper.fromPgWithState(user);
--- src/app/api/v1/auth/forgot-password/forgot-password.dto.ts
+++ src/app/api/v1/auth/forgot-password/forgot-password.dto.ts
@@ -1,6 +1,3 @@
-import { PasswordResetTokenResponse } from '@domain/base/password-reset-token/password-reset-token.response';
-import { UserResponse } from '@domain/base/user/user.response';
-import { ApiProperty } from '@nestjs/swagger';
 import z from 'zod';
 
 import { StandardResponse } from '@shared/http/http.response.dto';
@@ -18,30 +15,6 @@ export class ForgotPasswordDto extends zodDto(zod) {}
 
 // ========== Response ==========
 
-class ForgotPasswordDataRelationsPasswordResetToken {
-  @ApiProperty({ type: () => PasswordResetTokenResponse })
-  attributes: PasswordResetTokenResponse;
-}
-
-class ForgotPasswordDataUserRelations {
-  @ApiProperty({ type: () => ForgotPasswordDataRelationsPasswordResetToken })
-  passwordResetToken: ForgotPasswordDataRelationsPasswordResetToken;
-}
-
-class ForgotPasswordDataUser {
-  @ApiProperty({ type: () => UserResponse })
-  attributes: UserResponse;
-
-  @ApiProperty({ type: () => ForgotPasswordDataUserRelations })
-  relations: ForgotPasswordDataUserRelations;
-}
-
-class ForgotPasswordData {
-  @ApiProperty({ type: () => ForgotPasswordDataUser })
-  user: ForgotPasswordDataUser;
-}
-
 export class ForgotPasswordResponse extends StandardResponse {
-  @ApiProperty({ type: () => ForgotPasswordData })
-  data: ForgotPasswordData;
+  data: object;
 }
